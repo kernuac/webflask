6cb74df34af65c5726e018f801cffffaf09ba20d
Cristhian Vega Cortes <cristhian.vega.c@uda.cl>
2018-06-08 12:10:30 -0400

    adding flask as requirement

--- facsal/admin/controllers.py
+++ facsal/admin/controllers.py
@@ -2,14 +2,15 @@ from flask import Blueprint
 
 admin = Blueprint('admin', __name__)
 
+#/admin/
 @admin.route('/')
 def index():
     return "admin"
-
+#/admin/name
 @admin.route('/<name>/')
 def adminName(name):
     return name;
-
+#/admin/goldberg/asdf
 @admin.route('/goldberg/asdf')
 def goldbergASDF():
     return "goldberg asdf";
